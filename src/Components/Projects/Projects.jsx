cab762ac15ffc822717538f7844f854f34eebed3
boobalannaresh <bestforevermvi@gmail.com>
2023-08-01 23:56:22 +0530

    logo

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -11,7 +11,6 @@ import {
   SiSocketdotio,
   SiTailwindcss,
   SiReactrouter,
-  SiJavascript,
   SiMongodb,
   SiNodedotjs,
 } from "react-icons/si";
@@ -21,6 +20,9 @@ import { VscJson } from "react-icons/vsc";
 import { IoLogoJavascript } from "react-icons/io";
 import BookMyShow from "../../assets/BookMyShow.png";
 import pizza from "../../assets/pizza.png";
+import ChatApp from "../../assets/ChatApp.png";
+import SocialMedia from "../../assets/SocialMedia.png";
+
 
 export const Projects = () => {
   return (
@@ -28,29 +30,32 @@ export const Projects = () => {
       <div className="section">
         <h2 className="section__title different">Projects</h2>
         <div className="allProjects">
-          <div className="projects_container" data-aos="fade-right">
+
+        <div className="projects_container" data-aos="fade-right">
             <div className="project">
               <div className="project_videocontainer">
                 <div>
-                  <img src={BookMyShow} alt="Book MyShow Clone" />
+                  <img src={ChatApp} alt="Chat _ App" />
                 </div>
               </div>
               <div className="project_information">
-                <h2>Book MyShow Clone</h2>
+                <h2>Chat App</h2>
                 <p>
-                  Ticket booking is a process of selecting and making
-                  reservations for the cinema in online source.
+                  Create Chat app project using React functional components with React Hooks and Redux. 
+                  Using Socket.io instant, private chat sending a private message. If you will send a message, 
+                  you will get a notification
                 </p>
                 <div>
                   <FaReact color="#5bc0de" />
                   <BsBootstrap color="#59287a" />
                   <SiMongodb color="4DB33D" />
                   <SiNodedotjs color="#3c873a" />
-                  <SiExpress color="68A063"/>
+                  <SiExpress color="68A063" />
+                  <SiSocketdotio color="black"/>
                 </div>
                 <div>
                   <a style={{textDecoration:"none"}}
-                    href="https://github.com/boobalannaresh/guvi-hackathon2-frontend"
+                    href="https://github.com/boobalannaresh/Chat_app_frontend.git"
                     target="_blank"
                     rel="noreferrer"
                   >
@@ -59,7 +64,7 @@ export const Projects = () => {
                     </span>
                   </a>
                   <a style={{textDecoration:"none"}}
-                    href="https://github.com/boobalannaresh/guvi-hackathon2-backend"
+                    href="https://github.com/boobalannaresh/Chat_app_backend.git"
                     target="_blank"
                     rel="noreferrer"
                   >
@@ -68,7 +73,7 @@ export const Projects = () => {
                     </span>
                   </a>
                   <a style={{textDecoration:"none"}}
-                    href="https://guvi-hackathon2-frontend-ten.vercel.app/"
+                    href="https://chat-app-frontend-lilac.vercel.app/"
                     target="_blank"
                     rel="noreferrer"
                   >
@@ -84,7 +89,8 @@ export const Projects = () => {
               </div>
             </div>
           </div>
-          <div className="projects_container" data-aos="fade-right">
+
+        <div className="projects_container" data-aos="fade-right">
             <div className="project">
               <div className="project_videocontainer">
                 <div>
@@ -141,6 +147,124 @@ export const Projects = () => {
               </div>
             </div>
           </div>
+
+          <div className="projects_container" data-aos="fade-right">
+            <div className="project">
+              <div className="project_videocontainer">
+                <div>
+                  <img src={SocialMedia} alt="Social_media_clone" />
+                </div>
+              </div>
+              <div className="project_information">
+                <h2>Social_media</h2>
+                <p>
+                  Create social media project using React functional components with Context API and React Hooks. 
+                  Using Socket.io instant, private chat sending a private message and saving data in MongoDB
+                  Build a REST API with login, register, and all CRUD operations.
+                </p>
+                <div>
+                  <FaReact color="#5bc0de" />
+                  <SiMaterialui color="#03A9F4" />
+                  <SiMongodb color="4DB33D" />
+                  <SiNodedotjs color="#3c873a" />
+                  <SiExpress color="68A063" />
+                  <SiSocketdotio color="black"/>
+                </div>
+                <div>
+                  <a style={{textDecoration:"none"}}
+                    href="https://github.com/boobalannaresh/Social_media_clone_f.git"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt onbt">
+                      FrontEnd
+                    </span>
+                  </a>
+                  <a style={{textDecoration:"none"}}
+                    href="https://github.com/boobalannaresh/Social_media_clone_b.git"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt">
+                      BackEnd
+                    </span>
+                  </a>
+                  <a style={{textDecoration:"none"}}
+                    href="https://social-media-clone-frontend-navy.vercel.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span
+                      
+                      type="button"
+                      className="btns onbt"
+                    >
+                      Live
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="projects_container" data-aos="fade-right">
+            <div className="project">
+              <div className="project_videocontainer">
+                <div>
+                  <img src={BookMyShow} alt="Book MyShow Clone" />
+                </div>
+              </div>
+              <div className="project_information">
+                <h2>Book MyShow Clone</h2>
+                <p>
+                  Ticket booking is a process of selecting and making
+                  reservations for the cinema in online source.
+                </p>
+                <div>
+                  <FaReact color="#5bc0de" />
+                  <BsBootstrap color="#59287a" />
+                  <SiMongodb color="4DB33D" />
+                  <SiNodedotjs color="#3c873a" />
+                  <SiExpress color="68A063"/>
+                </div>
+                <div>
+                  <a style={{textDecoration:"none"}}
+                    href="https://github.com/boobalannaresh/guvi-hackathon2-frontend"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt onbt">
+                      FrontEnd
+                    </span>
+                  </a>
+                  <a style={{textDecoration:"none"}}
+                    href="https://github.com/boobalannaresh/guvi-hackathon2-backend"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt">
+                      BackEnd
+                    </span>
+                  </a>
+                  <a style={{textDecoration:"none"}}
+                    href="https://guvi-hackathon2-frontend-ten.vercel.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span
+                      
+                      type="button"
+                      className="btns onbt"
+                    >
+                      Live
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          
 
         </div>
       </div>
